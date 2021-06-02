97b83c2f6f19ec3b9311f69397fb3147a8643929
Paloca-RP <ricardopalhoca@hotmail.com>
2021-06-02 20:29:24 +0100

    Update ProjetoPOO.java

--- ProjetoPOO/src/projetopoo/ProjetoPOO.java
+++ ProjetoPOO/src/projetopoo/ProjetoPOO.java
@@ -4,10 +4,12 @@ public class ProjetoPOO {
     
     public static void main(String[] args) {
         //BEBIDAS
-        Bebida Imperial=new Bebida("Imperial",1,TaxaIva.VINTETRES,Tamanho.GRANDE,true);
-        Bebida Cola=new Bebida("Coca-Cola",1.40,TaxaIva.VINTETRES,Tamanho.MEDIO,false);
+        Bebida Imperial=new Bebida("Imperial",1.40,TaxaIva.VINTETRES,Tamanho.GRANDE,true);
+        Bebida Refrigerante=new Bebida("Refrigerante",1.40,TaxaIva.VINTETRES,Tamanho.MEDIO,false);
+        Bebida AguaPeq=new Bebida("Água",0.80,TaxaIva.SEIS,Tamanho.PEQUENO,false);
+        Bebida AguaMed=new Bebida("Água",1,TaxaIva.SEIS,Tamanho.MEDIO,false);
+        Bebida AguaGran=new Bebida("Água",1.10,TaxaIva.SEIS,Tamanho.GRANDE,false);
         
-        Bebida Agua=new Bebida("Água",0.70,TaxaIva.SEIS,Tamanho.PEQUENO,false);
         //DOCES
         Doce Mousse=new Doce("Mousse",1.50,TaxaIva.VINTETRES,"Mousse de Chocolate",true);
         Doce Pudim=new Doce("Pudim",1.50,TaxaIva.VINTETRES,"Pudim Flan",false);
@@ -24,8 +26,8 @@ public class ProjetoPOO {
         Prato DouradinhosPrato=new Prato("Douradinhos Prato",4.99,TaxaIva.VINTETRES,"Arroz ou Batatas, Salada e Molho.");
         Prato FalafelPrato=new Prato("Falafel Prato",4.99,TaxaIva.VINTETRES,"Arroz ou Batatas, Salada e Molho. 100%Vegetariano.");
         System.out.println(Imperial.toString());
-        System.out.println(Cola.toString());
-        System.out.println(Agua.toString());
+        System.out.println(Refrigerante.toString());
+        System.out.println(AguaPeq.toString());
     }
     
 }
